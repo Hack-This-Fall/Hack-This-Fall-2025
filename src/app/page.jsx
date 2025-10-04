a5e7ab9f516b45c4239d453b6e98b3a17131445a
Aryan Gajjar <gajjararyan1509@gmail.com>
2025-10-04 14:20:58 +0530

    feat: update partners section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,7 @@
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
+import VirtualHackathon from "../components/VirtualHackathon";
+import Partners from "@/components/Partner";
 
 export default function Home() {
   return (
@@ -7,6 +9,8 @@ export default function Home() {
       <Navbar />
       <main>
         <HeroSection />
+        <VirtualHackathon />
+        <Partners />
       </main>
     </div>
   );
